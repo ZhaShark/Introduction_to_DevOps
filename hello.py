0d7cf96e4bd4785e1619ea44375387e5a38e3ef6
white_cat <xy1091188310@gmail.com>
2025-10-05 10:48:32 +0100

    add vagrant configuration and flask app deployment

--- hello.py
+++ hello.py
@@ -7,23 +7,12 @@ def say_hello():
 	return '''
 	<p>Welcome!</p>
 	<p><a href ="/about">About this app</a></p>
-	<p><a href ="/contact">Contact me</a></p>
 	'''
 
 @app.route('/about')
 def about():
 	return '''
 	<p>This app is running on the Foasi web framework.</p>
-	<p><a href="https://flask.palletsprojects.com/" target="_blank">Learn more about Flask</a></p>
-	<p><a href="https://www.python.org/" target="_blank">Learn more about Python</a></p>
-	<p><a href="/">Back to Home</a></p>
-	<p><a href="/contact">Contact me</a></p>
-	'''
-@app.route('/contact')
-def contact():
-	return'''
-	<p>Please contact me if you have any qustion</p>
-	<p>mail:C23387203@mytudublin.ie</p>
-	<p><a href="/">Back to Home</a></p>
-	<p><a href="/about">About this app</a></p>
+	<p><a href="https://flask.palletsprojects.com/" target="_blank">Learn mmmore about Flask</a></p>
+	<p><a href="/">Home</a></p>
 	'''
